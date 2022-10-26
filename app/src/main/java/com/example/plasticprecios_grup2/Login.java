33031b833217a1bb06b391ec5675f11146b0ff5d
a14samvilcad <a14samvilcad@inspedralbes.cat>
2022-10-26 12:22:57 +0200

    BasicActivity cambia bien

---
+++ app/src/main/java/com/example/plasticprecios_grup2/Login.java
@@ -0,0 +1,26 @@
+package com.example.plasticprecios_grup2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Login extends AppCompatActivity {
+
+    private static final int TEXT_REQUEST = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+    public void launchInicio(View view) {
+
+        Intent intent = new Intent(this, inicio.class);
+        startActivityForResult(intent, TEXT_REQUEST);
+
+    }
+}
--- app/src/main/java/com/example/plasticprecios_grup2/inicio.java
+++ app/src/main/java/com/example/plasticprecios_grup2/inicio.java
@@ -13,23 +13,23 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import com.example.plasticprecios_grup2.databinding.ActivityHomeBinding;
+import com.example.plasticprecios_grup2.databinding.ActivityInicioBinding;
 
-public class activityHome extends AppCompatActivity {
+public class inicio extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
-    private ActivityHomeBinding binding;
+    private ActivityInicioBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        binding = ActivityInicioBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.toolbar);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_activity_home);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_inicio);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
@@ -44,7 +44,7 @@ public class activityHome extends AppCompatActivity {
 
     @Override
     public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_activity_home);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_inicio);
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
     }
