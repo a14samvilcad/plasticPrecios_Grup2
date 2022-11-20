7bc21db7a35957c9ba3e949e1b0e97b73e87ad86
a14samvilcad <a14samvilcad@inspedralbes.cat>
2022-11-21 00:24:01 +0100

    PROBAR LOGIN (casi terminado)

--- app/src/main/java/com/example/plasticprecios_grup2/Login.java
+++ app/src/main/java/com/example/plasticprecios_grup2/Login.java
@@ -1,28 +1,51 @@
 package com.example.plasticprecios_grup2;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.HashMap;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Login extends AppCompatActivity {
 
     private static final int TEXT_REQUEST = 1;
 
+    private Retrofit retrofit;
+    private RetrofitInterface retrofitInterface;
+    private String BASE_URL = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //Retrofits & Interfaces
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
+        retrofitInterface = retrofit.create(RetrofitInterface.class);
+
+
+        //Login Button
         findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -30,6 +53,7 @@ public class Login extends AppCompatActivity {
             }
         });
 
+        //New User Button
         findViewById(R.id.signup).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -38,10 +62,113 @@ public class Login extends AppCompatActivity {
         });
     }
 
-    private void handleSignupDialog() {
-    }
 
     private void handleLoginDialog(){
+        //CREO QUE HAY QUE PONER OTRA LAYOUT
+        View view = getLayoutInflater().inflate(R.layout.activity_login, null);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setView(view).show();
+
+        Button loginBtn = view.findViewById(R.id.login);
+        EditText userEdit = view.findViewById(R.id.textUser);
+        EditText passwordEdit = view.findViewById(R.id.textPassword);
+
+        //On click LOGIN
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                HashMap<String, String> map = new HashMap<>();
+
+                map.put("user", userEdit.getText().toString());
+                map.put("password", passwordEdit.getText().toString());
+
+                Call<LoginResult> call = retrofitInterface.executeLogin(map);
+
+                call.enqueue(new Callback<LoginResult>() {
+                    @Override
+                    public void onResponse(Call<LoginResult> call, Response<LoginResult> response) {
+                        //probar isSuccesful == true si no va
+                        if (response.code() == 200){
+
+                            LoginResult result = response.body();
+
+                            AlertDialog.Builder builder1 = new AlertDialog.Builder(Login.this);
+                            builder1.setTitle(result.getUser());
+                            builder1.setMessage(result.getPassword());
+
+                            builder1.show();
+
+                        }
+                        else if (response.code() == 404){
+                            Toast.makeText(Login.this, "Credenciales equivocados",
+                                    Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<LoginResult> call, Throwable t) {
+                        Toast.makeText(Login.this, t.getMessage(),
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+
+            }
+        });
+
+    }
+
+    private void handleSignupDialog() {
+
+        View view = getLayoutInflater().inflate(R.layout.signup_dialog, null);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(view).show();
+
+        Button signupBtn = view.findViewById(R.id.signup);
+        EditText nameEdit = view.findViewById(R.id.nameEdit);
+        EditText passwordEdit = view.findViewById(R.id.passwordEdit);
+
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                HashMap<String, String> map = new HashMap<>();
+
+                map.put("name", nameEdit.getText().toString());
+                map.put("password", passwordEdit.getText().toString());
+
+                Call<Void> call = retrofitInterface.executeSignUp(map);
+
+                call.enqueue(new Callback<Void>() {
+                    @Override
+                    public void onResponse(Call<Void> call, Response<Void> response) {
+
+                        if (response.code() == 200){
+                            Toast.makeText(Login.this,
+                                    "Inicio de sesion correcto", Toast.LENGTH_LONG).show();
+                        }
+                        else if (response.code() == 400){
+                            Toast.makeText(Login.this,
+                                    "Registrado", Toast.LENGTH_LONG).show();
+                        }
+
+
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<Void> call, Throwable t) {
+                        Toast.makeText(Login.this, t.getMessage(),
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
+
 
     }
 
--- app/src/main/java/com/example/plasticprecios_grup2/LoginResult.java
+++ app/src/main/java/com/example/plasticprecios_grup2/LoginResult.java
@@ -1,8 +1,27 @@
 package com.example.plasticprecios_grup2;
 
+import com.google.gson.annotations.SerializedName;
+
 public class LoginResult {
 
     private String user;
     private String password;
+
+
+
+
+
+
+
+
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
 
 }
